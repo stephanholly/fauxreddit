3b325b8529beb12ad725d633b9dbd7a87923f6a5
STEPHAN CHRISTOPHER HOLLY <stephan@axiomcoding.com>
2017-08-21 18:55:12 -0700

    minor bug fixes and styling

--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,17 @@ var bcrypt = require('bcrypt');
 router.get('/', function(req, res, next) {
   knex.raw(`select posts.title, posts.text, users.username, posts.id from posts inner join users on users.id = posts.author_id`)
   .then(function(data) {
-    res.render('index', {allPosts: data.rows, title:'Reddit but Better', cookie: req.cookies.user_id})
+    res.render('index', {allPosts: data.rows, title:'Faux Reddit', cookie: req.cookies.user_id})
 
 })
 });
 //get profile page
 router.get('/profile/:id', function(req,res,next) {
   if(req.cookies.user_id) {
-    knex.raw(`select * from users where id = ${req.cookies.user_id}`)
+    knex.raw(`select posts.title, posts.text, users.username from posts inner join users on users.id = posts.author_id where posts.author_id = ${req.cookies.user_id}`)
     .then(function(user) {
-      res.render('profile', {user: user.rows[0], title: 'Reddit but Better'})
+      res.render('profile', {user: user.rows, title: 'Faux Reddit'})
+      console.log(user.rows);
     })
   } else {
     res.redirect('/')
@@ -29,7 +30,7 @@ router.post('/', function(req, res, next) {
   .then(function(data1) {
     knex.raw(`select posts.title, posts.text, posts.id, users.username from posts inner join users on users.id = posts.author_id`)
   .then(function(data) {
-    res.render('index', {allPosts: data.rows, title:'Reddit but Better', cookie: req.cookies.user_id})
+    res.render('index', {allPosts: data.rows, title:'Faux Reddit', cookie: req.cookies.user_id})
     console.log(data.rows)
   })
   })
@@ -41,32 +42,27 @@ router.post('/', function(req, res, next) {
 router.get('/post/:id', function(req,res,next) {
     knex.raw(`select posts.title, posts.text, posts.id, users.username from posts inner join users on posts.author_id = users.id where posts.id = ${req.params.id}`)
     .then(function(data) {
-    knex.raw(`select comments.comment_text from comments where comments.original_post_id = ${req.params.id}`)
+    knex.raw(`select comments.comment_text, users.username from comments inner join users on comments.comment_author_id = users.id where comments.original_post_id = ${req.params.id} order by comments.id desc`)
     .then(function(data2) {
-      res.render('posts', {allPosts: data.rows[0], comment: data2.rows, title: 'Reddit but Better', cookie: req.cookies.user_id})
+      res.render('posts', {allPosts: data.rows[0], comment: data2.rows, title: 'Faux Reddit', cookie: req.cookies.user_id})
+      console.log(data2.rows);
     })
 })
 })
 //post comment
 router.post('/post/:id/comment', function(req,res,next) {
-  if(req.body.comText) {
+  if(req.body.comText !== '') {
   knex.raw(`insert into comments values (default, '${req.body.comText}', '${req.cookies.user_id}', '${req.params.id}')`)
   .then(function(data2) {
-  knex.raw(`select posts.title, posts.text, posts.id, users.username from posts inner join users on users.id = posts.author_id`)
-  .then(function(data) {
-  knex.raw(`select comments.comment_text from comments where comments.original_post_id = ${req.params.id}`)
-  .then(function(data4) {
-    res.render('posts', {title:'Reddit but Better', comment: data4.rows, cookie: req.cookies.user_id, allPosts: data.rows[0]})
+    res.redirect(`/post/${req.params.id}`)
   })
-})
-})
 } else {
   res.redirect('/')
 }
 });
 // get createAccount page
 router.get('/createAccount', function(req, res, next) {
-  res.render('createAccount', { title: 'Reddit but Better' });
+  res.render('createAccount', { title: 'Faux Reddit' });
 });
 // create new account
 router.post('/create', function(req, res, next) {
@@ -84,7 +80,7 @@ router.post('/create', function(req, res, next) {
 
 // get login page
 router.get('/login', function(req, res, next) {
-  res.render('login', { title: 'Reddit but Better' });
+  res.render('login', { title: 'Faux Reddit' });
 });
 
 // login
